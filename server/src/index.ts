3ce934e28e44ef655d9245653f86d8e1bb9bc961
Fletcher Dostie <fdostie@gmail.com>
2025-03-24 21:49:18 -0700

    add headless Phaser.Game instance to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,5 +11,46 @@ import { listen } from "@colyseus/tools";
 // Import arena config
 import appConfig from "./app.config";
 
+
+// imports for  phaser
+require('@geckos.io/phaser-on-nodejs')
+const { SnapshotInterpolation } = require('@geckos.io/snapshot-interpolation')
+const SI = new SnapshotInterpolation()
+const Phaser = require('phaser')
+
+
+class ServerScene extends Phaser.Scene {
+  constructor() {
+    super()
+    this.tick = 0
+  }
+
+  create() {
+    console.log("ServerScene created");
+  }
+
+  update() {
+    this.tick++
+  }
+}
+
+const config = {
+  type: Phaser.HEADLESS,
+  width: 1280,
+  height: 720,
+  banner: false,
+  audio: false,
+  scene: [ServerScene],
+  physics: {
+      default: "matter",
+      matter: {
+          debug: true,
+          gravity: { y: 0 }
+      }
+  },
+}
+
+new Phaser.Game(config)
+
 // Create and listen on 2567 (or PORT environment variable.)
 listen(appConfig);
