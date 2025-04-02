5e70ef9768555066eb7fe5fe63166c83404bba4f
Fletcher Dostie <fdostie@gmail.com>
2025-04-01 22:10:11 -0700

    attempt to hide URL bar

--- client/src/UI.ts
+++ client/src/UI.ts
@@ -584,6 +584,8 @@ export class GameUI {
   private updateUIPosition() {
     const canvas = document.querySelector('canvas') as HTMLCanvasElement;
     if (!canvas || !this.uiContainer) return;
+
+    this.setupScroll();
     
     this.uiContainer.style.top = canvas.offsetTop + 'px';
     this.uiContainer.style.left = canvas.offsetLeft + 'px';
@@ -1477,5 +1479,49 @@ export class GameUI {
     
     // Add to recent message container
     this.recentMessageContainer.appendChild(recentMessage);
+  }
+
+
+  private  setupScroll() {
+
+    var page = document.getElementById('page'),
+      ua = navigator.userAgent,
+      iphone = ~ua.indexOf('iPhone') || ~ua.indexOf('iPod'),
+      ipad = ~ua.indexOf('iPad'),
+      ios = iphone || ipad,
+      // Detect if this is running as a fullscreen app from the homescreen
+      fullscreen = window.navigator.standalone,
+      android = ~ua.indexOf('Android'),
+      lastWidth = 0;
+
+    if (android) {
+      // Android's browser adds the scroll position to the innerHeight, just to
+      // make this really fucking difficult. Thus, once we are scrolled, the
+      // page height value needs to be corrected in case the page is loaded
+      // when already scrolled down. The pageYOffset is of no use, since it always
+      // returns 0 while the address bar is displayed.
+      window.onscroll = function() {
+        page.style.height = window.innerHeight + 'px'
+      } 
+    }
+    // Start out by adding the height of the location bar to the width, so that
+    // we can scroll past it
+    if (ios) {
+      // iOS reliably returns the innerWindow size for documentElement.clientHeight
+      // but window.innerHeight is sometimes the wrong value after rotating
+      // the orientation
+      var height = document.documentElement.clientHeight;
+      // Only add extra padding to the height on iphone / ipod, since the ipad
+      // browser doesn't scroll off the location bar.
+      if (iphone && !fullscreen) height += 60;
+      page.style.height = height + 'px';
+    } else if (android) {
+      // The stock Android browser has a location bar height of 56 pixels, but
+      // this very likely could be broken in other Android browsers.
+      page.style.height = (window.innerHeight + 56) + 'px'
+    }
+    // Scroll after a timeout, since iOS will scroll to the top of the page
+    // after it fires the onload event
+    setTimeout(scrollTo, 0, 0, 1);
   }
 }
