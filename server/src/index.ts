2c8316cbcbf8487cfc22ff1767aeaa9751d67dca
Fletcher Dostie <fdostie@gmail.com>
2025-04-10 00:40:23 -0700

    just comment out profiling for now...

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,13 @@ import { listen } from "@colyseus/tools";
  
 // Import Colyseus config
 import app from "./app.config";
-import { startProfiling } from "./profile";
 
-// Start CPU profiling if enabled via env variable
-if (process.env.PROFILE === "true") {
-  startProfiling();
-}
+// import { startProfiling } from "./profile";
+
+// // Start CPU profiling if enabled via env variable
+// if (process.env.PROFILE === "true") {
+//   startProfiling();
+// }
  
 // Create and listen on 2567 (or PORT environment variable.)
 listen(app);
